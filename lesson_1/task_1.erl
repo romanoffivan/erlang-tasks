96bdf4146f094ea761f4ad4de9792a5acb37daf5
romanoffivan <iw.romanof@yandex.ru>
2019-12-17 14:01:20 +0300

    add task_2

--- lesson_1/task_1.erl
+++ lesson_1/task_1.erl
@@ -7,7 +7,7 @@
 
 %% Check is Val in range [From, To], both edges included
 is_in_range(Val, Left, Right) ->
-    (Val >= Left) and (Val =< Right).
+    Val >= Left andalso Val =< Right.
 
 is_in_range_test() ->
     ?assertEqual(true, is_in_range(5, 2, 10)),
