f8451bfa371bad2373417685297462993af3526e
romanoffivan <iw.romanof@yandex.ru>
2019-12-19 17:02:11 +0300

    add task_4

--- lesson_2/task_4.erl
+++ lesson_2/task_4.erl
@@ -8,7 +8,13 @@
 %% implement lists:dropwhile/2
 %% http://www.erlang.org/doc/man/lists.html#dropwhile-2
 dropwhile(Pred, List) ->
-    List.
+    case List of
+        [] -> [];
+        [Head | Tail] -> case Pred(Head) of
+                             true -> dropwhile(Pred, Tail);
+                             false -> List
+                         end
+    end.
 
 
 dropwhile_test() ->
@@ -25,7 +31,14 @@ dropwhile_test() ->
 %% implement lists:takewhile/2
 %% http://www.erlang.org/doc/man/lists.html#takewhile-2
 takewhile(Pred, List) ->
-    List.
+    case List of
+        [] -> [];
+        [Head | Tail] ->
+            case Pred(Head) of
+                true -> [Head | takewhile(Pred, Tail)];
+                false -> []
+            end
+    end.
 
 
 takewhile_test() ->
